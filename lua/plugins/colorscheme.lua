c3a97c2b96d7466be3972bdbeb91f436cb64ab3c
Quinn Stephan <quinn.stp@gmail.com>
2023-12-27 22:34:46 -0600

    Update colorscheme, add telescope file browser

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,8 +1,27 @@
 return {
-	'navarasu/onedark.nvim',
-	opts = { },
+	'EdenEast/nightfox.nvim',
 	init = function()
-		require('onedark').load()
+		vim.cmd('colorscheme carbonfox')
+
+		local palette = require('nightfox.palette').load('carbonfox')
+
+		local hl_groups = {
+			-- Telescope
+			{ 'TelescopeMatching', { fg = palette.orange.bright, underline = true } },
+			{ 'TelescopeSelection', { bg = palette.black.base } },
+			{ 'TelescopeNormal', { fg = palette.white.base, bg = palette.black.dim } },
+			{ 'TelescopePromptNormal', { bg = palette.black.base } },
+			{ 'TelescopeResultsBorder', { fg = palette.black.dim, bg = palette.black.dim } },
+			{ 'TelescopePreviewBorder', { fg = palette.black.dim, bg = palette.black.dim } },
+			{ 'TelescopePromptBorder', { fg = palette.black.base, bg = palette.black.base } },
+			{ 'TelescopePromptTitle', { fg = palette.black.base, bg = palette.blue.bright } },
+			{ 'TelescopeResultsTitle', { fg = palette.black.base, bg = palette.cyan.bright } },
+			{ 'TelescopePreviewTitle', { fg = palette.black.base, bg = palette.cyan.bright } },
+		}
+
+		for _, hl in pairs(hl_groups) do
+			vim.api.nvim_set_hl(0, hl[1], hl[2])
+		end
 	end,
 	lazy = false,
 	priority = 1000
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,10 +1,24 @@
 return {
-	'nvim-telescope/telescope.nvim',
-	tag = '0.1.5',
-	dependencies = { 'nvim-lua/plenary.nvim', 'nvim-tree/nvim-web-devicons' },
-	init = function()
-		local builtin = require('telescope.builtin')
-		vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-		vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-	end
+	{
+		'nvim-telescope/telescope.nvim',
+		tag = '0.1.5',
+		dependencies = { 'nvim-lua/plenary.nvim', 'nvim-tree/nvim-web-devicons' },
+		opts = {
+			defaults = {
+				sorting_strategy = 'ascending',
+				layout_strategy = 'horizontal',
+				layout_config = { prompt_position = 'top' },
+				border = true
+			}
+		},
+		init = function()
+			local builtin = require('telescope.builtin')
+			vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+			vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+		end
+	},
+	{
+		'nvim-telescope/telescope-file-browser.nvim',
+		dependencies = { 'nvim-telescope/telescope.nvim' }
+	}
 }
