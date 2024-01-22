4b6878222fa6b6f4b4a186717895e5603e4194b1
Quinn Stephan <quinn_stephan@bradycorp.com>
2024-01-22 10:59:04 -0600

    Add treesj

--- init.lua
+++ init.lua
@@ -1,6 +1,7 @@
 vim.g.mapleader = ' '
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
 vim.opt.number = true
 vim.opt.wrap = false
 vim.opt.relativenumber = true
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,12 +1,29 @@
 return {
-	'nvim-treesitter/nvim-treesitter',
-	build = ':TSUpdate',
-	opts = {
-		ensure_installed = { 'c', 'cpp', 'lua', 'vim', 'vimdoc', 'query' },
-		highlight = { enable = true },
-		indent = { enable = true }
+	{
+		'nvim-treesitter/nvim-treesitter',
+		build = ':TSUpdate',
+		opts = {
+			ensure_installed = { 'c', 'cpp', 'lua', 'vim', 'vimdoc', 'query' },
+			highlight = { enable = true },
+			indent = {
+				enable = true,
+				disable = { 'cpp' }
+			}
+		},
+		config = function(plugin, opts)
+			require('nvim-treesitter.configs').setup(opts)
+		end
 	},
-	config = function(plugin, opts)
-		require('nvim-treesitter.configs').setup(opts)
-	end
+	{
+		'Wansmer/treesj',
+		opts = {
+			use_default_keymaps = false
+		},
+		keys = function()
+			return {
+				{ '<leader>rs', require('treesj').split },
+				{ '<leader>rj', require('treesj').join }
+			}
+		end
+	}
 }
