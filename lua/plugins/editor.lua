4c8dd7c4418dae1183f61971dc432b5864447e90
Quinn Stephan <quinn_stephan@bradycorp.com>
2024-02-23 09:55:56 -0600

    Consolidate small files

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,18 +1,18 @@
 return {
-	{
-		'windwp/nvim-autopairs',
-		event = "InsertEnter",
-		opts = {}
-	},
-	{
-		'RRethy/vim-illuminate',
-		config = function(_, opts)
-			require('illuminate').configure(opts)
-		end,
-		opts = {
-			min_count_to_highlight = 2
-		}
-	},
+    {
+        'windwp/nvim-autopairs',
+        event = "InsertEnter",
+        opts = {}
+    },
+    {
+        'RRethy/vim-illuminate',
+        config = function(_, opts)
+            require('illuminate').configure(opts)
+        end,
+        opts = {
+            min_count_to_highlight = 2
+        }
+    },
     {
         'stevearc/dressing.nvim',
         opts = {
@@ -20,5 +20,21 @@ return {
                 border = 'none'
             }
         }
+    },
+    {
+        'numToStr/Comment.nvim',
+        opts = {},
+        lazy = false
+    },
+    {
+        'lukas-reineke/indent-blankline.nvim',
+        main = 'ibl',
+        opts = {}
+    },
+    {
+        "iamcco/markdown-preview.nvim",
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        ft = { "markdown" },
+        build = function() vim.fn["mkdp#util#install"]() end,
     }
 }
